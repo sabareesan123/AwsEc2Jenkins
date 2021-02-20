67975d5c31fb41a35bcf879991a958316e47e7da
sabareesan <sabareesanm@gmail.com>
2021-02-20 17:34:08 +0530

    changes done for external tomcat.Renamed the project to sync with git project name

--- AwsEc2TomcatJenkinsGithubMavenSpringBootJava11/src/main/java/com/example/AwsEc2TomcatJenkinsGithubMavenSpringBootJava11/rest_resorce/HelloResource.java
+++ AwsEc2TomcatJenkinsGithubMavenSpringBootJava11/src/main/java/com/example/AwsEc2TomcatJenkinsGithubMavenSpringBootJava11/rest_resorce/HelloResource.java
@@ -1,14 +1,14 @@
 package com.example.AwsEc2TomcatJenkinsGithubMavenSpringBootJava11.rest_resorce;
 
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
+@RequestMapping("api")
 public class HelloResource {
 	
-	@GetMapping("greet")
+	@GetMapping
 	public String greet()
 	{
 		return "hello";
